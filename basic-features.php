f6c9d8c384aeca32f3247371c1a595b6c92d348e
Collins Kiprono <one.kcollin@gmail.com>
2022-09-14 10:48:40 +0300

    Initial commit

---
+++ basic-features.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name:       Basic Plugin
+ * Plugin URI:        https://kprono.pro/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Collins Kiprono
+ * Author URI:        https://kprono.pro/
+ * License:           GPL v2 or later
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+
+?>
