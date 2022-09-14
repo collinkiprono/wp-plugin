dc3f68920aa1b29f2cf846d16f6ca1906b628cc2
Collins Kiprono <one.kcollin@gmail.com>
2022-09-14 11:56:42 +0300

    Set up Webpack, Composer and ESLint

--- basic-features.php
+++ basic-features.php
@@ -2,15 +2,14 @@
 /**
  * Plugin Name:       Basic Plugin
  * Plugin URI:        https://kprono.pro/
- * Description:       Handle the basics with this plugin.
- * Version:           1.10.3
+ * Description:       Simple WordPress plugin to handle basics.
+ * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Collins Kiprono
  * Author URI:        https://kprono.pro/
- * License:           GPL v2 or later
- * Text Domain:       my-basics-plugin
+  * License URI:      https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       basic-plugin
  * Domain Path:       /languages
  */
 
-?>
